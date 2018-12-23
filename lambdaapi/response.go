c8ba3a77df833a6385ba03a1d07437500735062b
hbc <bcxxxxxx@gmail.com>
2018-12-23 17:12:47 +0800

    feat(lambdaapi): add lambda api gateway response helpers

---
+++ lambdaapi/response.go
@@ -0,0 +1,36 @@
+package lambdaapi
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func BadRequest() (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 400,
+	}, nil
+}
+
+func ErrorResponse(err error) (*events.APIGatewayProxyResponse, error) {
+	resp, _ := JSONResponse(map[string]string{
+		"error": err.Error(),
+	})
+	resp.StatusCode = 500
+	return resp, nil
+}
+
+func JSONResponse(thing interface{}) (*events.APIGatewayProxyResponse, error) {
+	b, err := json.Marshal(thing)
+	if err != nil {
+		return nil, err
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(b),
+	}, nil
+}
