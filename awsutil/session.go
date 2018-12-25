21cc7870cf51900b2b7b3ea260e5cdc354554b98
hbc <bcxxxxxx@gmail.com>
2018-12-26 01:19:50 +0800

    [WIP]

--- awsutil/session.go
+++ awsutil/session.go
@@ -6,15 +6,20 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func MustCreateSession() *session.Session {
 	s, err := session.NewSession(&aws.Config{
-		Region: &aws.String(os.Getenv("YOUDONT_AWS_REGION")),
+		Region: aws.String(os.Getenv("YOUDONT_AWS_REGION")),
 	})
 	if err != nil {
 		panic(fmt.Errorf("create session: %v", err))
 	}
 
 	return s
+}
+
+func MustAcquireDynamoDB(s *session.Session) *dynamodb.DynamoDB {
+	return dynamodb.New(s)
 }
--- model/post.go
+++ model/post.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
 
 // Post represents a post.
 type Post struct {
@@ -9,4 +14,33 @@ type Post struct {
 	Content   string                 `json:"content"`
 	Meta      map[string]interface{} `json:"meta"`
 	CreatedAt time.Time              `json:"created_at"`
+}
+
+type PostRepository interface {
+	QueryLatest(n int) ([]*Post, error)
+	Add(*Post) error
+}
+
+type postRepository struct {
+	db        *dynamodb.DynamoDB
+	tableName string
+}
+
+func MakePostRepository(db *dynamodb.DynamoDB) *postRepository {
+	return &postRepository{
+		db:        db,
+		tableName: "youdont_post",
+	}
+}
+
+func (p *postRepository) QueryLatest(n int) ([]*Post, error) {
+	q := &dynamodb.ScanInput{
+		TableName: aws.String(p.tableName),
+	}
+
+	return nil, nil
+}
+
+func (p *postRepository) Add(*Post) error {
+	return nil
 }
